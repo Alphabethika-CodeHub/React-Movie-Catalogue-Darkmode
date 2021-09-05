3801113751e007832fb13dac9ad51024fd7cc490
Alphabethika-CodeHub <Artemisia512@gmail.com>
2021-09-05 19:29:34 +0700

    Remove Unused Nav List

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,7 +12,6 @@ const Navbar = () => {
                 <a className="mr-4" href="#">Popular</a>
                 <a className="mr-4" href="#">Upcoming</a>
                 <a className="mr-4" href="#">Liked</a>
-                <a className="mr-4" href="#">Liked</a>
             </div>
         </div>
     );
